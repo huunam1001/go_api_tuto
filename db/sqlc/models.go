f1aef3ff56e183a1aceba927b8b5e3da2dfb6634
nam.nh <huunam101@gmail.com>
2023-02-20 17:29:45 +0700

    change json tag

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -13,22 +13,22 @@ type Account struct {
 	Owner     string    `json:"owner"`
 	Balance   int64     `json:"balance"`
 	Currency  string    `json:"currency"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 type Entry struct {
 	ID        int64 `json:"id"`
-	AccountID int64 `json:"account_id"`
+	AccountID int64 `json:"accountID"`
 	// can be negative or positive
 	Amount    int64     `json:"amount"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 type Transfer struct {
 	ID            int64 `json:"id"`
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
+	FromAccountID int64 `json:"fromAccountID"`
+	ToAccountID   int64 `json:"toAccountID"`
 	// must be positive
 	Amount    int64     `json:"amount"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"createdAt"`
 }
