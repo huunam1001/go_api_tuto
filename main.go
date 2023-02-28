1c6e35ad4c77e788ed072df06fb2d88381528352
nam.nh <huunam101@gmail.com>
2023-02-28 15:34:19 +0700

    first add mongo db

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"go_api_tuto/api"
 	db "go_api_tuto/db/sqlc"
 	"go_api_tuto/util"
+	"time"
 
 	_ "github.com/lib/pq"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"log"
 )
 
 func main() {
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://huunam1001:ninhhuunam@cluster0.kcqlk9o.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println("MongoDB FAIL")
+		log.Fatal(err)
+	} else {
+		fmt.Println("MongoDB connected")
+	}
+	defer client.Disconnect(ctx)
 
 	config, err := util.LoadConfig(".")
 
